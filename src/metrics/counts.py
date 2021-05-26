0705a39694b842a15a1ba509aaadf532c3b7b0df
Chris Simpkins <git.simpkins@gmail.com>
2021-05-25 22:18:36 -0400

    document parameters in docstrings

--- src/metrics/counts.py
+++ src/metrics/counts.py
@@ -4,6 +4,6 @@
 def vocabulary_size(tokens):
     """Returns the vocabulary size count defined as the number of alphabetic
     characters as defined by the Python str.isalpha method. This is a
-    case-sensitive count."""
+    case-sensitive count. `tokens` is a list of token strings."""
     vocab_list = set(token for token in tokens if token.isalpha())
     return len(vocab_list)
--- src/metrics/ratios.py
+++ src/metrics/ratios.py
@@ -3,5 +3,6 @@
 
 def lexical_diversity(tokens):
     """Returns a *case-sensitive* lexical diversity measure.  We want to keep case forms
-    of the same word as these are considered different tokens in this corpus."""
+    of the same word as these are considered different tokens in this corpus. `tokens`
+    is a list of token strings."""
     return len(set(tokens)) / len(tokens)
