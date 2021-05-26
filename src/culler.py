c86e44903bb144c40e04a71d00c5e8ac4d154e4d
Chris Simpkins <git.simpkins@gmail.com>
2021-05-25 21:39:21 -0400

    [culler.py] add new empty content exclusion criterion, refactor approach to stub checks

--- src/culler.py
+++ src/culler.py
@@ -13,6 +13,13 @@ from rich.console import Console
 
 from meta import __version__
 
+# tuple of localized `title` attribute defintions in Wikipedia stub
+# article HTML `a href` tags
+WIKI_STUB_TITLES = {
+    "en": "Wikipedia:Stub",  # English
+    "he": "ויקיפדיה:קצרמר",  # Hebrew
+}
+
 
 async def async_read(filepath):
     """Asynchronous IO text file reads"""
@@ -20,26 +27,32 @@ async def async_read(filepath):
         return await f.read()
 
 
-async def parse_content(json_text):
-    """Asynchronous content text parsing"""
-    json_obj = json.loads(json_text)
-    html_text = json_obj["parse"]["text"]
-    soup = BeautifulSoup(html_text, "lxml")
-    contents = soup.find_all("p")
-    content_list = []
-    for content in contents:
-        content_list.append(content.text)
-
-    return "\n".join(content_list)
-
-
 # ==================
 # Exclusion criteria
 # ==================
 
 
-async def is_exclusion_stub(text):
-    return "is a stub" in text
+async def is_exclusion_stub(json_text, lang_tag):
+    if lang_tag in WIKI_STUB_TITLES:
+        json_obj = json.loads(json_text)
+        html_text = json_obj["parse"]["text"]
+        soup = BeautifulSoup(html_text, "lxml")
+        stub_title = WIKI_STUB_TITLES[lang_tag]
+        # if we identify the stub HTML tag, it is a stub
+        if soup.find("a", {"title": stub_title}):
+            return True
+
+    return False
+
+
+async def is_exclusion_empty_content(json_text):
+    json_obj = json.loads(json_text)
+    html_text = json_obj["parse"]["text"]
+    soup = BeautifulSoup(html_text, "lxml")
+    if len(soup.find_all("p")) == 0:
+        return True
+    else:
+        return False
 
 
 # ==================
@@ -52,31 +65,35 @@ async def remove_file(filepath):
     os.remove(filepath)
 
 
-async def remove_files(filepaths):
+async def remove_files(filepaths, lang_tag):
     """Asynchronous file removal based on inclusion/exclusion criteria."""
     remove_path_list = []
     stub_file_count = 0
+    empty_content_count = 0
 
     for filepath in filepaths:
         json_text = await async_read(filepath)
-        content_text = await parse_content(json_text)
         # exclusion criterion: Is a stub file
-        if await is_exclusion_stub(content_text):
+        if await is_exclusion_stub(json_text, lang_tag):
             remove_path_list.append(filepath)
             stub_file_count += 1
+
+        if await is_exclusion_empty_content(json_text):
+            remove_path_list.append(filepath)
+            empty_content_count += 1
 
     # remove files that meet exclusion criteria
     for remove_filepath in remove_path_list:
         await remove_file(remove_filepath)
 
-    removed_file_count = stub_file_count
+    removed_file_count = stub_file_count + empty_content_count
     return removed_file_count
 
 
-def cull(filepaths):
+def cull(filepaths, lang_tag):
     """Asynchronous file removal based on inclusion/exclusion criteria"""
     loop = asyncio.get_event_loop()
-    removed_file_count = loop.run_until_complete(remove_files(filepaths))
+    removed_file_count = loop.run_until_complete(remove_files(filepaths, lang_tag))
     return removed_file_count
 
 
@@ -90,6 +107,7 @@ def main(argv):
     parser.add_argument(
         "--version", action="version", version=f"culler.py v{__version__}"
     )
+    parser.add_argument("LANGTAG", help="Language tag")
     parser.add_argument("TARGETDIR", help="Write directory path")
     args = parser.parse_args(argv)
 
@@ -111,7 +129,7 @@ def main(argv):
         console.print(f"Start file count: {start_filepath_count}")
 
         # cull files according to inclusion/exclusion criteria
-        removed_file_count = cull(filepaths)
+        removed_file_count = cull(filepaths, args.LANGTAG)
 
         # file removal report
         remaining_files = len(list(Path(args.TARGETDIR).glob("*.json")))
