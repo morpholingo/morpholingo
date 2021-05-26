266c63e48f79de41a3f36890c046e793bb93618f
Chris Simpkins <git.simpkins@gmail.com>
2021-05-25 22:46:03 -0400

    add parsers.html module with parse_html_p_content function

---
+++ src/parsers/html.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+
+
+def parse_html_p_content(html_text):
+    """Parses HTML <p> tag text content and returns as
+    a newline-delimited concatenated string.  Parsed
+    with BeautifulSoup 4.  Returns either a string or
+    None (no content)"""
+    soup = BeautifulSoup(html_text, "lxml")
+    contents = soup.find_all("p")
+    if len(contents) > 0:
+        return "\n".join(contents)
+    else:
+        return None
