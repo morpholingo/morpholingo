9f76fc58f04922081ff4313da40cf62f7c0f466c
Chris Simpkins <git.simpkins@gmail.com>
2021-05-25 22:17:21 -0400

    add metrics.counts module with vocabulary_size function

---
+++ src/metrics/counts.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def vocabulary_size(tokens):
+    """Returns the vocabulary size count defined as the number of alphabetic
+    characters as defined by the Python str.isalpha method. This is a
+    case-sensitive count."""
+    vocab_list = set(token for token in tokens if token.isalpha())
+    return len(vocab_list)
